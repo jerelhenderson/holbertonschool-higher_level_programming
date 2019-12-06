1dcea5bad448e9edae4c2abe9bdebda479823458
Jerel Henderson <habbitnijuu@hotmail.com>
2019-12-06 17:15:07 +0000

    Initial Commit 0x01 Python If-Else Loops Functions, Dec. 5

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def islower(c):
+    for letter in range(93, 123):
+        if ord(c) == letter:
+            return True
+        else:
+            return False
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    lastdigit = int(repr(n)[-1])
+    return lastdigit
