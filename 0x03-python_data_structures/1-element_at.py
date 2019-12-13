1d8eafd8fd183bfbecb64479d8623b72259b6971
Jerel <henderson.jerel@yahoo.com>
2019-12-13 19:16:06 +0000

    Initial Commit 0x03 Python Data Structures, Dec. 12

--- 0x03-python_data_structures/1-element_at.py
+++ 0x03-python_data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def element_a(my_list, idx):
-if idx < 0:
-return
-if idx >= (my_list):
-return
-return my_list[idx]
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > len(my_list) + 1:
+        return None
+    else:
+        return my_list[idx]
