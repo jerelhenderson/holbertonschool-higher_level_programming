fbfdc6b85e8cfa4ee1d3fbf2ed4bb7ea1c59a274
Jerel Henderson <habbitnijuu@hotmail.com>
2019-12-18 18:39:46 +0000

    Fix a couple mistakes

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def number_keys(my_dict):
-    return len(list(my_dict.keys))
+    return len(list(my_dict.keys()))
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(my_dict, key, value):
-        if key in my_dict:
-                my_dict[key] = value
+        my_dict[key] = value
         return my_dict
