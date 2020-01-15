7e39a3e95fe63ce0d31522f778f82e02b8354603
Jerel <henderson.jerel@yahoo.com>
2020-01-15 14:48:24 +0000

    Add uniitest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,3 +1,31 @@
 #!/usr/bin/python3
 import unittest
 max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_pos_integers(self):
+        self.assertEqual(max_integer([10, 20, 30, 40, 50]), 50)
+
+    def test_neg_integers(self):
+        self.assertEqual(max_integer([-50, -40, -30, -20, -10]), -10)
+
+    def test_floats(self):
+        self.assertEqual(max_integer([1.1, 2.2, 3.3, 4.4, 5.5]), 5.5)
+
+    def test_strings(self):
+        self.assertEqual(max_integer("Holberton"), 't')
+
+    def test_no_input(self):
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_invalid_input(self):
+        with self.assertRaises(TypeError):
+            max_integer((1, "bob"))
+
+    def test_mixed_ints(self):
+        self.assertEqual(max_integer([-50.5, -50, 0, 50, 50.5]), 50.5)
+
+
+if __name__ == '__main__':
+    unittest.main()
