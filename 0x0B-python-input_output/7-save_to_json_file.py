b2fdb151baff3314820abc5f4df4d0a049ec7d4f
Jerel <henderson.jerel@yahoo.com>
2020-01-18 18:28:03 +0000

    pep8 fixes

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import json
 
+
 def save_to_json_file(my_obj, filename):
     with open(filename, "w", encoding="UTF8") as my_json:
         json.dump(my_obj, my_json)
--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import json
 
+
 def load_from_json_file(filename):
     with open(filename, "r", encoding="UTF8") as my_json:
         return(json.load(my_json))
