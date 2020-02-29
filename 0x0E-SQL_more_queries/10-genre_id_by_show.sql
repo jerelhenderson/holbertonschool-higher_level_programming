030d9dd9619dc539f87a312f6b6e1aaf2f39a4b5
Jerel Henderson <henderson.jerel@yahoo.com>
2020-02-29 19:37:28 +0000

    More quick fixes

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- list all shows in database 'hbtn_0d_tvshows'
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
-INNER JOIN tv_show_genres ON tv_shows.id='tv_show_genres.show_id'
+-- list all shows in database 'hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
