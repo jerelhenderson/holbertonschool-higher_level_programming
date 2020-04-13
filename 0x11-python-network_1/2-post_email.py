68f1fa7bae9808d9e62d42240f643621dc606290
Jerel Henderson <henderson.jerel@yahoo.com>
2020-04-13 16:55:01 +0000

    pep8 and fix 2-post_email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     url = argv[1]
     email = {'email]': argv[2]}
     data = parse.urlencode(email)
-    req = request.urlopen.read()
-    with 
-    print (response.read().decode("utf8"))
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as response:
+        print (response.read().decode("utf8"))
--- 0x11-python-network_1/3-error_codes.py
+++ 0x11-python-network_1/3-error_codes.py
@@ -14,4 +14,4 @@ if __name__ = "__main__":
             print(response.read().decode("utf8"))
     except:
         error.HTTPError as error:
-    print("Error code: {}".format(error.code))
+        print("Error code: {}".format(error.code))
