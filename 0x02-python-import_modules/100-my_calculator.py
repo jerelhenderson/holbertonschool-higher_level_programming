b802aeeea74a4c189e5281f4f6e4feb79edfe6a7
Jerel Henderson <henderson.jerel@yahoo.com>
2020-04-14 07:58:45 +0000

    Modify files, pep8

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ from sys import argv
 if __name__ in "__main__":
     if len(argv) != 4:
         print ("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
 
     a = int(argv[1])
     b = int(argv[3])
@@ -20,4 +20,4 @@ if __name__ in "__main__":
         print ("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print ("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
