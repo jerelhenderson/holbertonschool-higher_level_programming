1f5cddaf45e667a985931ef385312dc5996dae55
Jerel Henderson <henderson.jerel@yahoo.com>
2020-04-22 14:54:47 +0000

    Fix Exception errors

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,19 +23,22 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) == tuple:
-            if not value[0] < 0 or value[1] < 0:
-                if len(value) == 2:
-                    if type(value[0]) == int or type(value[1]) == int:
-                        self.__position = value
+        if type(value) is not tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
         else:
-            raise TypeError("position must be a tuple of 2 positive integers")
+            self.__position = value
 
     def area(self):
         return(self.__size * self.__size)
 
     def my_print(self):
-        if (self.__size == 0):
+        if (self.__size < 0):
             print("")
             return
 
